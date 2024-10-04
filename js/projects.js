cd2a47fbd85d40f8d147e48047270fbe043f9ade
karankhullar126 <75215765+karankhullar126@users.noreply.github.com>
2024-10-05 00:18:36 +0530

    Inital commit

---
+++ js/projects.js
@@ -0,0 +1,75 @@
+fetch('projects.json', {
+    method: 'GET',
+    mode:"no-cors" // or 'POST' if needed
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok ' + response.statusText);
+        }
+        return response.json();
+    })
+    .then(data => {
+        const itemList = document.getElementById('portfolio-list');
+        const items = data.projects; // Access the items array
+
+        // Loop through the items and create div elements
+        for (let i = 0; i < items.length; i++) {
+            // Card
+            const cardPortfoliio = document.createElement('div');
+            cardPortfoliio.className = 'card-portfolio' 
+            // Youtube
+            if(items[i]['youtube']) {
+                let frame = document.createElement('iframe')
+                frame.src = items[i].youtube
+                frame.className = 'image-view'
+                cardPortfoliio.appendChild(frame)
+            } else {
+                // Image
+                const img = document.createElement('img');
+                img.src = items[i].image;
+                img.className = 'image-view'
+                cardPortfoliio.appendChild(img)
+            }
+            
+            // card content
+            const cardContent = document.createElement('div')
+            cardContent.className = "card-content"
+            cardPortfoliio.appendChild(cardContent)
+            // Title
+            const title = document.createElement('h2')
+            title.className = "card-title-portfolio"
+            title.textContent = items[i].title
+            cardContent.appendChild(title)
+            // Description
+            const description = document.createElement('p')
+            description.className = 'card-description'
+            description.innerHTML = items[i].description
+            cardContent.appendChild(description)
+
+            // App Store
+            if(items[i]['appstoreLink']) {
+                let anchorTag = document.createElement('a')
+                anchorTag.href = items[i].appstoreLink
+                
+                const appStoreImg = document.createElement('img')
+                appStoreImg.src = 'images/svg/AppStore.svg'
+                appStoreImg.className = 'appStore'
+
+                anchorTag.appendChild(appStoreImg)
+                cardContent.appendChild(anchorTag)
+            }
+
+            
+           
+
+            itemList.appendChild(cardPortfoliio)
+
+            // const itemDiv = document.createElement('div');
+            // itemDiv.className = 'item';
+            // itemDiv.textContent = items[i];
+            // itemList.appendChild(itemDiv);
+        }
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
